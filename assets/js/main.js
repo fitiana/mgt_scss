69ee72216e6d1922e8db0c3367ab11ec0482b92f
faly.medias <faly.medias@gmail.com>
2022-08-11 15:59:08 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+jQuery(function($) {
+    'use strict';
+
+  
+
+    // Sticky Nav
+    $(window).on('scroll', function() {
+        $(window).scrollTop() >= 100 ?
+        $('.navbar-area').addClass('stickyadd') :
+        $('.navbar-area').removeClass('stickyadd');
+    });
+
+    // Smooth Scrolling
+    $('a.nav-link').on('click', function(e) {
+        var $this = $(this);
+        $('html, body')
+        .stop()
+        .animate({
+            scrollTop: $($this.attr('href')).offset().top - 60,
+        }, 1000 );
+        e.preventDefault();
+    });
+
+    // Search Popup
+    $('.search-btn').on('click', function() {
+        $('.search-popup').toggle(300);
+    });
+
+  
+
+    // Preloader
+    $(window).on('load', function(e) {
+        $('.preloader-main')
+        .delay(2000)
+        .queue(function() {
+            $(this).remove();
+        });
+    });
+
+}(jQuery));
